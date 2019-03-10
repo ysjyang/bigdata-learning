63e3d83ea0f9c37eb56532fac098f1b6761352a3
yangshj <yangshj@beagledata.com>
2019-03-10 15:22:57 +0800

    转换算子demo

--- sparkStu/src/main/scala/com/netcloud/bigdata/spark_core/basiclearning/transform/Transform_0020_TransformPractice.scala
+++ sparkStu/src/main/scala/com/netcloud/bigdata/spark_core/basiclearning/transform/Transform_0020_TransformPractice.scala
@@ -1,13 +1,19 @@
 package com.netcloud.bigdata.spark_core.basiclearning.transform
 
+
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * 准换算子实战
+  * 转换算子实战
   * map：将集合中的所有元素乘以2
   * filter：过滤出集合中的所有的偶数
   * flatMap：将文本行拆分多个单词
   * groupByKey：将班级的成绩分组
+  * reduceByKey:统计每个班级的总分数
+  * sortByKey：对班级的分数进行排序 默认是升序排序
+  * join:打印每个学生的成绩
+  * cogroup:打印每个学生的成绩
+  *
   * @author yangshaojun
   * #date  2019/3/9 17:08
   * @version 1.0
@@ -17,10 +23,14 @@ object Transform_0020_TransformPractice {
 
     val conf = new SparkConf().setAppName("TransFormationPractice").setMaster("local[2]")
     val sc = new SparkContext(conf)
-//    map(sc)
-//    filter(sc)
-//    flatMap(sc)
-    groupByKey(sc)
+    //    map(sc)
+    //    filter(sc)
+    //    flatMap(sc)
+    //    groupByKey(sc)
+    //    reduceByKey(sc)
+    //    sortByKey(sc)
+    //    join(sc)
+    corgroup(sc)
     sc.stop()
 
   }
@@ -46,20 +56,20 @@ object Transform_0020_TransformPractice {
     * @param sc
     */
   def filter(sc: SparkContext): Unit = {
-    val numlist = List(1, 2, 3, 4, 5,6,7,8,9,10);
+    val numlist = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     val numberRDD = sc.parallelize(numlist)
     /**
       * 使用map算子;将集合中的每个元素都乘以2
       */
-    val multipleNumberRDD = numberRDD.filter(_ %2==0)
+    val multipleNumberRDD = numberRDD.filter(_ % 2 == 0)
     multipleNumberRDD.foreach(println)
   }
 
   /**
     * flatMap：将文本行拆分多个单词
     */
-  def flatMap(sc: SparkContext): Unit ={
-    val numlist = List("hello you","hello me","hello world");
+  def flatMap(sc: SparkContext): Unit = {
+    val numlist = List("hello you", "hello me", "hello world");
     val numberRDD = sc.parallelize(numlist)
     /**
       * 使用map算子;将集合中的每个元素都乘以2
@@ -71,8 +81,8 @@ object Transform_0020_TransformPractice {
   /**
     * groupByKey：按照班级对成绩分组
     */
-  def groupByKey(sc: SparkContext): Unit ={
-    val numlist = List(Tuple2("class1",90),Tuple2("class1",92),Tuple2("class2",50),Tuple2("class2",60));
+  def groupByKey(sc: SparkContext): Unit = {
+    val numlist = List(Tuple2("class1", 90), Tuple2("class1", 92), Tuple2("class2", 50), Tuple2("class2", 60));
     val numberRDD = sc.parallelize(numlist)
     /**
       * 使用map算子;将集合中的每个元素都乘以2
@@ -80,4 +90,91 @@ object Transform_0020_TransformPractice {
     val multipleNumberRDD = numberRDD.groupByKey()
     multipleNumberRDD.foreach(println)
   }
+
+  /**
+    * reduceByKey:统计每个班级的总分数
+    *
+    * @param sc
+    */
+  def reduceByKey(sc: SparkContext): Unit = {
+    val numlist = List(Tuple2("class1", 90), Tuple2("class1", 92), Tuple2("class2", 50), Tuple2("class2", 60));
+    val numberRDD = sc.parallelize(numlist)
+    val retRDD = numberRDD.reduceByKey(_ + _)
+    retRDD.foreach(println)
+
+  }
+
+  /**
+    * sortByKey：对班级的分数进行排序
+    */
+  def sortByKey(sc: SparkContext): Unit = {
+    val numlist = Array(Tuple2(65, "Tom"), Tuple2(50, "jack"), Tuple2(100, "luce"), Tuple2(85, "ff"));
+    val scoreRDD = sc.parallelize(numlist)
+    val retValuRDD = scoreRDD.sortByKey(false)
+    retValuRDD.collect.foreach(println)
+  }
+
+  /**
+    * join:打印每个学生的成绩
+    */
+  def join(sc: SparkContext): Unit = {
+    val students = Array(
+      Tuple2(1, "tom"),
+      Tuple2(2, "jack"),
+      Tuple2(3, "marry"),
+      Tuple2(4, "ff"))
+
+    val scores = Array(
+      Tuple2(1, 80),
+      Tuple2(2, 90),
+      Tuple2(3, 100),
+      Tuple2(1, 60),
+      Tuple2(2, 80))
+
+    val studentRDD = sc.parallelize(students)
+    val scoreRDD = sc.parallelize(scores)
+    val retRDD = studentRDD.join(scoreRDD)
+    retRDD.foreach(println)
+
+    /**
+      * (1,(tom,80))
+      * (2,(jack,90))
+      * (1,(tom,60))
+      * (2,(jack,80))
+      * (3,(marry,100))
+      */
+
+  }
+
+  /**
+    * corgroup:打印每个学生的成绩
+    */
+  def corgroup(sc: SparkContext): Unit = {
+    val students = Array(
+      Tuple2(1, "tom"),
+      Tuple2(2, "jack"),
+      Tuple2(3, "marry"),
+      Tuple2(4, "ff"))
+
+    val scores = Array(
+      Tuple2(1, 80),
+      Tuple2(2, 90),
+      Tuple2(3, 100),
+      Tuple2(1, 60),
+      Tuple2(2, 80))
+
+    val studentRDD = sc.parallelize(students)
+    val scoreRDD = sc.parallelize(scores)
+    val retRDD = studentRDD.cogroup(scoreRDD)
+    retRDD.foreach(println)
+
+    /**
+      * (4,(CompactBuffer(ff),CompactBuffer()))
+      * (1,(CompactBuffer(tom),CompactBuffer(80, 60)))
+      * (2,(CompactBuffer(jack),CompactBuffer(90, 80)))
+      * (3,(CompactBuffer(marry),CompactBuffer(100)))
+      */
+  }
+
+
 }
